ab0f0b72c687a8daa7c96cc74d2518f4660ca348
Iurii Sylaiev - LenovoGG <iuriisylaiev@gmail.com>
2025-02-13 22:01:36 +0200

    fixed (Func swithed to Object methods)

--- HW 7.1. Сума вартості послуг.js
+++ HW 7.1. Сума вартості послуг.js
@@ -1,8 +1,38 @@
 // Опис ДЗ: Припустимо, у нас є об'єкт, властивості якого — назва послуги та її вартість:
-var services = {
+const services = {
     "стрижка": "60 грн",
     "гоління": "80 грн",
-    "Миття голови": "100 грн"
+    "Миття голови": "100 грн",
+
+        // Створити метод price(), який обчислює та повертає загальну вартість наданих послуг.
+        price() {
+            return Object.values(this).reduce((sum, value) => {
+                if (typeof value === 'string' && value.includes('грн')) {
+                    return sum + parseInt(value);
+                }
+                return sum;
+            }, 0);
+        },
+
+        // Створити метод minPrice(), який повертає мінімальну ціну.
+        minPrice() {
+            return Math.min(...Object.values(this).map(value => {
+                if (typeof value === 'string' && value.includes('грн')) {
+                    return parseInt(value);
+                }
+                return Infinity; // Infinity завжди більше за будь-яке інше число, воно не буде враховане при обчисленні мінімальної ціни.
+            }));
+        },
+
+        // Створити метод maxPrice(), який повертає максимальну ціну.
+        maxPrice() {
+            return Math.max(...Object.values(this).map(value => {
+                if (typeof value === 'string' && value.includes('грн')) {
+                    return parseInt(value);
+                }
+                return -Infinity;  //-Infinity завжди менше за будь-яке інше число, воно не буде враховане при обчисленні максимальної ціни.
+            }));
+        }
     };
     
     // Послуги можуть додаватися по ходу роботи:
@@ -12,24 +42,8 @@ var services = {
     services['Анекдот'] = "20 грн";
     services['Маникюр'] = "299 грн";
     
-    // Створити метод price(), який обчислює та повертає загальну вартість наданих послуг.
     
-    function price(services) {
-        return Object.values(services).reduce((sum, value) => sum + parseInt(value), 0);
-    }
     
-    // Створити метод minPrice(), який повертає мінімальну ціну.
-    
-    function minPrice(services) {
-        return Math.min(...Object.values(services).map(value => parseInt(value)));
-    }
-    
-    // Створити метод maxPrice(), який повертає максимальну ціну.
-    
-    function maxPrice(services) {
-        return Math.max(...Object.values(services).map(value => parseInt(value)));
-    }
-    
-    console.log(`загальна вартість наданих послуг: ${price(services)} грн`)
-    console.log(`мінімальна ціна послуги: ${minPrice(services)} грн`)
-    console.log(`максимальна ціна послуги: ${maxPrice(services)} грн`)
+    console.log(`загальна вартість наданих послуг: ${services.price()} грн`)
+    console.log(`мінімальна ціна послуги: ${services.minPrice()} грн`)
+    console.log(`максимальна ціна послуги: ${services.maxPrice()} грн`)
